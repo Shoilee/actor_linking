a067976195838a1e927b3c3cb9a2066e483daaa3
Shoilee <sarahshoilee121@gmail.com>
2024-02-04 19:26:39 +0100

    re-run exp300

--- deezymatch/fuzzy_string_matching.py
+++ deezymatch/fuzzy_string_matching.py
@@ -10,32 +10,35 @@ def read_file(file):
 
 
 # TODO add more languages; consider the language spoken by dutch colony or just specify I did it for two language
-def retrieve_uri_from_label(label):
-    g = rdflib.Graph()
+def retrieve_uri_from_label(label, df=pandas.read_pickle("data/ground_truth.pkl")):
+    wiki_uri_list = []
 
-    # match with any language, now match only with dutch and english language
+    for i, row in df.iterrows():
+        if label in row['wiki_label']:
+            wiki_uri_list.append(row['wiki_uri'])
 
-    wikidata_query = f"""
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX wd: <http://www.wikidata.org/entity/>
-        PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-
-        select * where {{ 
-            SERVICE <https://query.wikidata.org/sparql> {{
-                ?human wdt:P31 wd:Q5 .
-                {{?human rdfs:label "{label.replace('"', '')}"@nl}}
-                UNION
-                {{?human rdfs:label "{label.replace('"', '')}"@en}}
-            }}
-        }}
-        """
-    # print(wikidata_query)
-
-    return [constituent['human'] for constituent in g.query(wikidata_query)]
+    return list(set(wiki_uri_list))
 
 
-def run(source_file, destination_file, directory='data/deezymatch/'):
-    name_to_id = pandas.read_pickle(directory+'name_to_id.pickle')
+def run(source_file, destination_file, directory='../data/'):
+    df = pandas.read_pickle("data/ground_truth.pkl")
+
+    # DICTIONARY MAPS ID TO ALL DIFFERENT NAME VARIANTS
+    id_to_names = dict()
+    for i, row in df.iterrows():
+        id = row['wiki_uri'].toPython()
+        altnames = list(set([row['name_label']] + [label for label in row["wiki_label"]]))
+        id_to_names[id] = altnames
+
+    # DICTIONARY MAPS NAME VARIATION TO ITS ID
+    name_to_id = dict()
+    for id in id_to_names:
+        for name in id_to_names[id]:
+            if name in name_to_id:
+                name_to_id[name].append(id)
+            else:
+                name_to_id[name] = [id]
+
     candidate_df = pandas.read_pickle(source_file, compression='infer')
 
     try:
